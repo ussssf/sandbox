f262d62c5551389da03f9aa68831a67723361b36
Hajjaji Youssef <90615406+ussssf@users.noreply.github.com>
2024-06-21 11:40:26 +0100

    Update app.py

--- ats/app.py
+++ ats/app.py
@@ -1,24 +1,31 @@
 import pandas as pd
 import plotly.graph_objects as go
 
-# Example data
+# Example data with null values
 data = [
     {
         "Combination": ["ProductA", "Variation1"],
         "runids": ["101", "102", "103"]
     },
     {
-        "Combination": ["ProductB", "Variation2"],
+        "Combination": ["ProductB", None],
         "runids": ["104", "105", "106"]
     },
     {
-        "Combination": ["ProductC", "Variation3"],
+        "Combination": [None, "Variation3"],
         "runids": ["107", "108", "109"]
+    },
+    {
+        "Combination": [None, None],
+        "runids": ["110", "111"]
     }
 ]
 
 # Convert data to a DataFrame
 df = pd.DataFrame(data)
+
+# Replace None values with empty strings
+df['Combination'] = df['Combination'].apply(lambda x: [i if i is not None else '' for i in x])
 df['Combination'] = df['Combination'].apply(lambda x: ' - '.join(x))
 df['runids'] = df['runids'].apply(lambda x: ', '.join(x))
 
